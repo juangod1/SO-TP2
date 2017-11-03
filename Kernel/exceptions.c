3b4fcd858d00c2a26cbc32070059e06ba2493940
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-11-03 18:54:42 -0300

    Added exceptions ID

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,11 +1,18 @@
 
 #define ZERO_EXCEPTION_ID 0
+#define OVERFLOW_EXCEPTION_ID 4
+#define INVALID_OPCODE_EXCEPTION_ID 6
 
 static void zero_division();
 
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
+
+	else if (exception == INVALID_OPCODE_EXCEPTION_ID)
+		op_code();
+
+	else if (exception == OVERFLOW_EXCEPTION_ID)
 }
 
 static void zero_division() {
