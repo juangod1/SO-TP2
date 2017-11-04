1ed43efac51b4b48b306400873718dd6903cf005
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-11-04 17:11:33 -0300

    removed else if statement

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -8,9 +8,6 @@ static void zero_division();
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
-
-	else if (exception == INVALID_OPCODE_EXCEPTION_ID)
-		op_code();
 }
 
 static void zero_division() {
