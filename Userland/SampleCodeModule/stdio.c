926740c3fab451fe4f8cec61310b602f4b43fbba
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-11-08 18:22:39 -0300

    toNum is now divided into toInt and toFloat
    toFloat needs fixing!!!

--- Userland/SampleCodeModule/stdio.c
+++ Userland/SampleCodeModule/stdio.c
@@ -7,14 +7,26 @@ int strcmp(const char* s1, const char* s2){
 	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
-int isNum(char* s) {
+int isNum(char * s) {
+	int decimal_marks = 0;
+
 	if (*s == '-') {
 		s++;
 	}
 
+	if (!IS_DIGIT(*s)) {
+		return 0;
+	} else {
+		s++;
+	}
+
 	while (*s != '\0') {
-		if ((*s - '0') < 0 || (*s - '0') > 9) {
-			return 0;
+		if (!IS_DIGIT(*s)) {
+			if (*s == '.' && decimal_marks < 1 && IS_DIGIT(*(s + 1))) {
+				decimal_marks++;
+			} else {
+				return 0;
+			}
 		}
 
 		s++;
@@ -23,11 +35,11 @@ int isNum(char* s) {
 	return 1;
 }
 
-int toNum(char* string) {
+int toInt(char * string) {
 	int length;
 	int powerTo;
 	int rta = 0;
-	char* auxString = string;
+	char * auxString = string;
 
 	if (*string == '-') {
 		string++;
@@ -41,6 +53,43 @@ int toNum(char* string) {
 		rta += n * tenPow(powerTo);
 		string++;
 		powerTo--;
+	}
+
+	return (*auxString == '-') ? -rta : rta;
+}
+
+float toFloat(char * string) {
+	int int_length;
+	int decimal_length;
+	int total_length;
+	int powerTo;
+	float rta = 0;
+	char * auxString = string;
+
+	if (*string == '-') {
+		string++;
+	}
+
+	int_length = subStrleng(string, '.');
+	decimal_length = strleng(string + 1 + int_length);
+	total_length = int_length + 1 + decimal_length;
+	powerTo = int_length;
+	sysPrintInt(decimal_length, 0, 255, 255);
+	sysPrintString("\n", 0, 255, 255);
+
+	for (int i = 0 ; i < total_length; i++) {
+		int n = *string - '0';
+
+		if (i < int_length) {
+			powerTo--;
+			rta += (n * tenPow(powerTo));
+		} else if (i > int_length && (i - (int_length + 1)) < decimal_length) {
+			// The + 1 stands for the decimal mark
+			powerTo++;
+			rta += (n / tenPow(powerTo));
+		}
+
+		string++;
 	}
 
 	return (*auxString == '-') ? -rta : rta;
--- Userland/SampleCodeModule/stdio.h
+++ Userland/SampleCodeModule/stdio.h
@@ -3,6 +3,7 @@
 
 int strcmp(const char* s1, const char* s2);
 int isNum(char* s);
-int toNum(char* string);
+int toInt(char* string);
+float toFloat(char * string);
 
 #endif
