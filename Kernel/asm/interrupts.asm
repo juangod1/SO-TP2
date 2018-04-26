10b2e9448b1c544df2cd9d3e94306631950ef668
fdelgado <fdelgado@itba.edu.ar>
2018-04-26 17:40:32 -0300

    Add timer tick scheduling handler

--- Kernel/asm/interrupts.asm
+++ Kernel/asm/interrupts.asm
@@ -20,6 +20,7 @@ GLOBAL _generalProtection
 
 EXTERN irqDispatcher
 EXTERN exceptionDispatcher
+EXTERN schedule
 EXTERN printInt
 SECTION .text
 
@@ -137,3 +138,61 @@ haltcpu:
 
 SECTION .bss
 	aux resq 1
+
+
+%macro POPAQ
+	pop rip
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx
+	pop rbp
+	pop rdi
+	pop rsi
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	pop fs
+	pop gs
+%endmacro
+
+
+
+%macro PUSHAQ
+	push gs
+	push fs
+	push r15
+	push r14
+	push r13
+	push r12
+	push r11
+	push r10
+	push r9
+	push r8
+	push rsi
+	push rdi
+	push rbp
+	push rdx
+	push rcx
+	push rbx
+	push rax
+	push rip
+%endmacro
+
+
+
+_tick_handler:
+	PUSHAQ
+	mov rdi,rsp
+	;optionally change to kernel stack here for security reasons.
+	call schedule 
+	mov rsp, rax
+	POPAQ
+	mov al, 20h
+	out 20h, al
+	iretq
