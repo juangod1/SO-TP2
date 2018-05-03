e9d785ab755a59a4ab9bdc559eac85c01fa47eb3
juangod1 <juan.godfrid@gmail.com>
2018-05-03 17:50:00 -0300

    Addresses 0x400000-0x799999 are now free to use for mem manager

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -15,8 +15,8 @@ extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x1000;
 
-static void * const sampleCodeModuleAddress = (void*)0x400000;
-static void * const sampleDataModuleAddress = (void*)0x500000;
+static void * const sampleCodeModuleAddress = (void*)0x800000;
+static void * const sampleDataModuleAddress = (void*)0x900000;
 
 typedef int (*EntryPoint)();
 
