0fe1c5398263b1730c9e7d00e9106bcb1f09bc6f
pabloito <pradnic@itba.edu.ar>
2018-05-11 17:10:05 -0300

    Checkpoint DJango

--- Kernel/semaphore.c
+++ Kernel/semaphore.c
@@ -37,28 +37,17 @@ int taskRequest(semaphore sem, int pid) //eventually will have to ask pid type
 {
   if(semaphoreCheck(&(sem->value)))
   {
-    printString("SemaphoreCheck returns true!\n",0,255,0);
     return 0;
   }
-    printString("SemaphoreCheck returns false!\n",0,0,255);
   sem->value--;
-  printString("Pid: ",0,255,0);
-  printInt(pid,0,255,0);
-  printString("\n",0,0,0);
   processQueueAdd(pid, &(sem->processQueue));
-  printString("before sleep, PID: \n",30,60,90);
-  printInt(pid,30,60,90);
   sleepProcess(pid);
-  printString("After sleep.\n",255,255,255);
   return 0;
 }
 
 void taskFinished(semaphore sem, int pid) //eventually will have to ask for pid type
 {
   sem->value++;
-  printString("Sem value in task finished:",255,0,255);
-  printInt(sem->value,255,0,255);
-  printString("\n", 255, 0, 255);
   if(sem->value <1)
   {
     pid_t newProcess=processQueueRemove(&(sem->processQueue));
